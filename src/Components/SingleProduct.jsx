2440fad622ed8cadf8e24d25a4ef2e10b066b87a
Atif Maqsood <atifmaqsood907@gmail.com>
2023-02-23 01:17:31 +0500

    Add a functionality to fetch products from mongoDB

--- src/Components/SingleProduct.jsx
+++ src/Components/SingleProduct.jsx
@@ -7,18 +7,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { ButtonGroup } from "react-bootstrap";
-import myImg from "../images/f1.jpeg"
 
-function SingleProduct() {
-
+function SingleProduct({ product }) {
+  const productImg = product.img ? product.img.replace(/\\/g, "/") : "";
   return (
     <>
       <Card>
-      <Card.Img variant="top" src={myImg} />
+      <Card.Img variant="top" src={`http://localhost:5000/${productImg}`} />
 
         <Card.Body>
-          <Card.Title>Dummy Title</Card.Title>
-          <Card.Text>Rs 323</Card.Text>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>Rs{product.price}</Card.Text>
           <ButtonGroup>
             <Button variant="primary" className="mx-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Add to Cart">
               <FontAwesomeIcon icon={faCartArrowDown} /> 
